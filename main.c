86b4e0ea43de0f896ea01af6ae93510b3f071637
Shantanu Jadhav <70745640+AIBappa@users.noreply.github.com>
2026-03-14 16:44:53 +0530

    updates to acquire region and language from mobile

--- main.c
+++ main.c
@@ -443,7 +443,7 @@ int generate_firmware_sign(char* signfile) {
 
     char json_request[2048];
     memset(json_request, 0, sizeof(json_request));
-    int len = snprintf(json_request, sizeof(json_request), "{\n\t\"d\" : \"%s\",\n\t\"v\" : \"%s\",\n\t\"c\" : \"%s\",\n\t\"b\" : \"%s\",\n\t\"sn\" : \"%s\",\n\t\"r\" : \"GL\",\n\t\"l\" : \"en-US\",\n\t\"f\" : \"1\",\n\t\"id\" : \"\",\n\t\"options\" : {\n\t\t\"zone\" : %s\n\t},\n\t\"pkg\" : \"%s\"\n}", codename, version, codebase, branch, serial_num, romzone, pkg_hash);
+    int len = snprintf(json_request, sizeof(json_request), "{\n\t\"d\" : \"%s\",\n\t\"v\" : \"%s\",\n\t\"c\" : \"%s\",\n\t\"b\" : \"%s\",\n\t\"sn\" : \"%s\",\n\t\"r\" : \"%s\",\n\t\"l\" : \"%s\",\n\t\"f\" : \"1\",\n\t\"id\" : \"\",\n\t\"options\" : {\n\t\t\"zone\" : %s\n\t},\n\t\"pkg\" : \"%s\"\n}", codename, version, codebase, branch, serial_num, region, lang, romzone, pkg_hash);
     free(pkg_hash);
 
     if (len < 0 || len >= (int)sizeof(json_request)) {
